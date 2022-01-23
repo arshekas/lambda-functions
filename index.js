b71ab3b342899d5ae0a9423f07e3373266acd208
arshekas <singh366arsh@gmail.com>
2022-01-23 15:26:55 -0800

    finishing up

--- index.js
+++ index.js
@@ -8,8 +8,6 @@ import {
   Context,
 } from 'aws-lambda';
 
-import * as _ from 'lodash';
-
 AWS.config.update({ region: 'ca-central-1' });
 
 const host = 'xenum.cdhd2xlqunl4.ca-central-1.rds.amazonaws.com';
@@ -43,16 +41,36 @@ const response = {
 
 async function processRequest(events) {
   try {
-    // getting the rank and limit from body
-    const { rank } = events.body;
-    const { limit } = events.params;
+    // **Connection to database**
+    connection.connect(function (err) {
+      if (err) {
+        console.error('Database connection failed: ' + err.stack);
+        context.fail();
+        return;
+      } else {
+        console.log('Connected to database.');
+        // getting the rank and limit from body
+        const { rank } = events.body;
+        const { limit } = events.params;
 
-    // return users according to the rank and limit
-    const response = await knex('users')
-      .select('*')
-      .orderBy(rank, 'desc')
-      .limit(limit);
-    return response;
+        // return users according to the rank and limit
+        const response = await knex('users')
+          .select('*')
+          .orderBy(rank, 'desc')
+          .limit(limit);
+        return response;
+      }
+    });
+    //Send API Response
+    callback(null, {
+      statusCode: '200',
+      body: 'succeed',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    //Close Connection
+    connection.end(); // Missing this section will result in timeout***
   } catch (error) {
     console.log(error);
     response.statusCode = 500;
